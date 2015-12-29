848b633dac741b7b8416f3fcdbff611a328e875b
Виктор <viktortat@gmail.com>
2015-12-29 08:00:24 +0200

    1

---
+++ Ef6Test1/Ef6Test1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ef6Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EFDbFirstEntities db = new EFDbFirstEntities())
+            {
+                var list = db.Agents.ToList();
+                foreach (Agent ag in list)
+                {
+                    Console.WriteLine("{0} - {1} - {2} - {3} - {4}",ag.Id,ag.FirstName,ag.LastName,ag.Age,ag.Descr);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
